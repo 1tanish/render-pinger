0b4c99af2e18a379b7be242bbc64976bc6c2f2c6
zero <826474nilwan@gmail.com>
2025-09-02 00:57:40 +0530

    changed url

--- api/ping.js
+++ api/ping.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
   try {
-    const response = await fetch("https://portfoli0-9n2k.onrender.com/get-todos");
+    const response = await fetch("https://skelenet.onrender.com//get-todos");
     const todos = await response.json();
     return res.status(200).json(todos);
   } catch (err) {
